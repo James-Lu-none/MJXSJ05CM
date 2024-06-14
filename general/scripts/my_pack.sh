2757ff4fe3f3375655955670064635b224b6e935
James-Lu-none <jamesabcde277@gmail.com>
2024-06-15 02:29:50 +0800

    chore: Update my_pack.sh script to allocate more space for the release image

--- general/scripts/my_pack.sh
+++ general/scripts/my_pack.sh
@@ -8,7 +8,7 @@ rootfs=rootfs.squashfs.$chip
 
 release=openipc-$chip-nor-$variant.bin
 
-dd if=/dev/zero bs=1K count=5000 status=none | tr '\000' '\377' > $release
+dd if=/dev/zero bs=1K count=8192 status=none | tr '\000' '\377' > $release
 dd if=output/images/$uboot of=$release bs=1K seek=0 conv=notrunc status=none
 dd if=output/images/$uImage of=$release bs=1K seek=320 conv=notrunc status=none
 dd if=output/images/$rootfs of=$release bs=1K seek=2368 conv=notrunc status=none
